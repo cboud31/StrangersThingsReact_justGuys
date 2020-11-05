a7a673af602e90cdee32a55bfb01121d38c66e9a
Brian Rogers <macdaddy@Brians-MacBook-Pro.local>
2020-11-05 15:19:32 -0800

    update thursday nov, 6

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export { default as Search } from "./Search";
-export { default as Posts } from "./Posts";
-export { default as User } from "./User";
+//export { default as Search } from "./Search";
+//export { default as Posts } from "./Posts";
+//export { default as User } from "./User";
+export { default as Auth } from "./Auth";
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Posts, Search, User } from "./components";
+
+import { getToken, clearToken } from "./api";
+
+import { Posts, Search, User, Auth } from "./components";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!getToken());
+
   return (
     <div className="app">
-      <Posts />
+      {isLoggedIn ? (
+        <>
+          <h1>Thanks for logging in!</h1>
+          <button
+            onClick={() => {
+              clearToken();
+              setIsLoggedIn(false);
+            }}
+          >
+            LOG OUT
+          </button>
+        </>
+      ) : (
+        <Auth setIsLoggedIn={setIsLoggedIn} />
+      )}
+
+      {/*<Posts />
       <Search />
-      <User />
+      <User />*/}
     </div>
   );
 };
